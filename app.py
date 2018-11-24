4a9d8bd78c3606cc67d9de4b1f45097e5bb43e28
majaku <m.jakubowski.git@gmail.com>
2018-11-24 13:45:20 +0100

    correct typo in db link

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqllite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.secret_key = 'superKEYsuperSECRETWHATOUT'
 api = Api(app)
 
