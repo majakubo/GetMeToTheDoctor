db7358b32f8cc36345d00f1ed942551ad8366de4
majaku <m.jakubowski.git@gmail.com>
2018-11-24 14:58:17 +0100

    implemented GetMe post endpoint

--- resources/navigation.py
+++ resources/navigation.py
@@ -5,14 +5,14 @@ class GetMe(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('from',
                         type=dict,
-                        required=True,
                         help='Send starting point qr/room_name')
     parser.add_argument('to',
                         type=dict,
-                        required=True,
                         help='Send ending point qr/room_name')
 
     def post(self):
+        print("in post")
         data = GetMe.parser.parse_args()
         print("you start at {} which is {}".format(data['from']['value'], data['from']['type']))
         print("you end at {} which is {}".format(data['to']['value'], data['to']['type']))
+        return data
